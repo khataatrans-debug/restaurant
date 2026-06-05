1fef9d49bc506f147b7ba686a1d9802e90674ebd
khataatrans-debug <info.aatrans@gmail.com>
2026-06-05 14:54:48 +0700

    sua app

--- sw.js
+++ sw.js
@@ -1,15 +1,23 @@
-const CACHE = 'tms-v1';
+const CACHE = 'driver-v1';
 const ASSETS = [
-  '/landing.html',
-  '/login.html',
+  '/driver.html',
   '/app-config.js',
   '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/icon-192.png'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
